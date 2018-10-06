8b8646341e07466a172e20bb1947dd51b6b37509
Shivani <shghatge@gmail.com>
2018-10-06 18:55:51 -0400

    World 0 Passed

--- lab2/bug_2.py
+++ lab2/bug_2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 import tf
 import time
@@ -5,6 +7,25 @@ import math
 from geometry_msgs.msg import Twist,  Point,  Quaternion
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
+from rbx1_nav.transform_utils import quat_to_angle, normalize_angle
+
+
+def get_min_range_right():
+    global ranges
+    distance_min=9999
+    for i in range( 320):
+        if (distance_min>ranges[i]):
+            distance_min=ranges[i]
+    return (distance_min)
+
+'''def get_min_range_right():
+    global ranges
+    distance_min=9999
+    for i in range(len(ranges)/2):
+        if (distance_min>ranges[640-i-1]):
+            distance_min=ranges[640-i-1]
+               
+    return (distance_min)'''
 
 # Callback function copies the distance of obstacle right in front and at the right most angle of view
 def scan_callback(msg):
@@ -12,8 +33,13 @@ def scan_callback(msg):
     global front_range, right_range 
     front_range = msg.ranges[ len( msg.ranges ) / 2 ]
     right_range = msg.ranges[-1]
-
+    global ranges
+    ranges = msg.ranges
+    right_range = get_min_range_right()
+    
 #callback function to get the current position and pose of the robot
+
+
 
 def get_odom():
     try:
@@ -21,7 +47,9 @@ def get_odom():
     except  (tf.Exception,  tf.ConnectivityException,  tf.LookupException):
         rospy.loginfo( "TF Exception" )
         return
-    return  Point(trans[0], trans[1], trans[2]), rot
+
+    print("Positions "+str(trans[0])+" "+str(trans[1]))
+    return  Point(trans[0], trans[1], trans[2]), quat_to_angle(Quaternion(*rot))
 
 # def odom_callback(odom_data):
 #     # print("Inside Odometry callback")
@@ -30,36 +58,42 @@ def get_odom():
 #     robot_pose = odom_data.pose.pose.orientation
 
 def translate_robot(distance):
-    global rate
-    move_cmd.linear.x = 0.1
-    linear_duration = distance / 0.1
+    global rate, robot_pos, robot_pose
+
+    move_cmd = Twist()
+    move_cmd.linear.x = 0.2
+    linear_duration = distance / 0.2
     # Move for a time to go the desired distance
-    ticks = int(linear_duration * rate)
+    ticks = int(linear_duration * ros_rate)
 
     for t in range(ticks):
-        self.cmd_vel.publish(move_cmd)
+        cmd_vel.publish(move_cmd)
         rate.sleep()
 
     move_cmd = Twist()
-    self.cmd_vel.publish(move_cmd)
+    cmd_vel.publish(move_cmd)
     rospy.sleep(1) 
+    robot_pos, robot_pose = get_odom()
+    print(robot_pose)
 
-def rotate_robot(angle):
+def rotate_robot(angle, direction):
     global rate, angle_turned
-    angle_turned += angle
 
-    move_cmd.angular.z = 2
-    angular_duration = angle / 2
+    #angle_turned += ((direction) * angle)
+    print("Rotating by angle "+str(angle))
+    move_cmd = Twist()
+    move_cmd.angular.z = direction * 1
+    angular_duration = angle / 1
     # Move for a time to turn to the desired angle
-    ticks = int(angular_duration * rate)
+    ticks = int(angular_duration * ros_rate)
 
     for t in range(ticks):
-        self.cmd_vel.publish(move_cmd)
+        cmd_vel.publish(move_cmd)
         rate.sleep()
 
     # Stop the robot 
     move_cmd = Twist()
-    self.cmd_vel.publish(move_cmd)
+    cmd_vel.publish(move_cmd)
     rospy.sleep(1)  
 
 def isGoalReached():
@@ -73,61 +107,80 @@ def isGoalReached():
     return val
 
 def is_m_line():
-    val = True
-    if ( (robot_pos.y < -0.1) or (robot_pos.y > 0.1) ):
-        val = False
-    if ( (robot_pos.z < -0.1) or (robot_pos.z > 0.1) ):
-        val = False
+    val = False
+    print("M_Line Check : Pos "+str(robot_pos.x)+" "+str(robot_pos.y))
+
+    if ( (robot_pos.y >= -0.1) and (robot_pos.y <= 0.1) ):
+	if(robot_pos.x > hit_point.x):
+	    val = True
+    	    print("M_Line Check is Hit: Pos "+str(robot_pos.x)+" "+str(robot_pos.y))
+    
     return val
 
 def is_hit_point():
     val = False
 
-    if ( abs(robot_pos.y - hit_point.y) <= 0.2 and abs(robot_pos.x - hit_point.x) <= 0.2):
+    if ( abs(robot_pos.y - hit_point.y) <= 0.2 and abs(robot_pos.x - hit_point.x) < 0.05):
         val = True
+	print("HitPoint Check is Hit: Pos "+str(robot_pos.x)+" "+str(robot_pos.y))
 
     return val
 
 def isObstacleAhead():
 
-    if( math.isnan(front_range) == True)
+    if( math.isnan(front_range) == True):
         return False
 
-    if (front_range > 0.5)
+    if (front_range > 1.0):
         return False
 
     return True
 
 def follow_wall():
+    global impossible
+    print("Following wall")
 
-    while( math.isnan(right_range) == False)
-        rotate_robot(2)
+    while( right_range < 9999):
+        print(" rotate until right is NAN Ranges "+str(front_range) +" "+str(right_range))
+        rotate_robot(1, 1)
 
-    while ( is_m_line() == True or is_hit_point() == True):
-        translate_robot(0.1)
+    while ( True ):
+        
+        print(" Move forward Ranges "+str(front_range) +" "+str(right_range))
+	translate_robot(translate_speed)
 
-        if( math.isnan(right_range) == False):
-            rotate_robot(-2)
-        else:
-            rotate_robot(2)
-            translate_robot(0.1)
+        if( right_range > 3.0):
+            rotate_robot(1, -1)
 
-    if (is_m_line() == True ):
-        rotate_robot(-angle_turned)
+        if( right_range < 0.8):
+            rotate_robot(1, 1)
 
-    if (is_hit_point() == True):
-        impossible = True
+        if (is_m_line() == True ):
+	    print("M line is hit, should turn "+str(robot_pose))
+	    if(robot_pose < 0):
+                rotate_robot(abs(robot_pose), 1)
+	    else:
+		rotate_robot(abs(robot_pose), -1)
+	    break
 
+        if (is_hit_point() == True):
+            impossible = True
+	    break
 
 
 
 
 
 # These variables are updated as the robot moves, initialised to arbitrary values
-front_range = 1.0
-right_range = 0.8
+front_range = 9999
+right_range = 9999
+translate_speed = 0.2
 hit_point = None
 impossible = False
+robot_pose = None
+robot_pos = Point(0, 0, 0)
+
+ros_rate = 50
 
 angle_turned = 0
 
@@ -135,6 +188,7 @@ angle_turned = 0
 #robot_pos = None
 #robot_pose = None
 
+rospy.init_node('bug_2', anonymous = False)
 tf_listener  =  tf.TransformListener()
 odom_frame  =  '/odom'
 
@@ -142,10 +196,8 @@ odom_frame  =  '/odom'
 
 scan_top = rospy.Subscriber('scan', LaserScan, scan_callback)
 # odo_top = rospy.Subscriber('odom', Odometry, odom_callback)
-cmd_vel_pub = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size = 100)
-rate = rospy.Rate(20)
-
-rospy.init_node('bug_2', anonymous = False)
+cmd_vel = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size = 2)
+rate = rospy.Rate(ros_rate)
 
 try:
     tf_listener.waitForTransform( odom_frame,  '/base_footprint' ,  rospy.Time(),  rospy.Duration( 1.0 ))
@@ -153,18 +205,22 @@ try:
 except(tf.Exception,  tf.ConnectivityException,  tf.LookupException):
     rospy.loginfo( "Cannot find transform between /odom and /base_link or /base_footprint" )
     rospy.signal_shutdown( "tf Exception" )
-    robot_pos, robot_pose = get_odom()
+    #robot_pos, robot_pose = get_odom()
     
 # This loop runs until the goal is reached
 while (isGoalReached() == False):
-    robot_pos, robot_pose = get_odom()
+    print("Goal not reached")
+    #robot_pos, robot_pose = get_odom()
 
     if (isObstacleAhead() == False):
-        translate_robot(0.1)
+        print(" Ranges "+str(front_range) +" "+str(right_range))
+	print("Obstace not ahead")
+        translate_robot(translate_speed)
     else:
         hit_point = robot_pos
         hit_point.x = hit_point.x + front_range
-        print("Obstacle Hit")
+        print(" Ranges "+str(front_range) +" "+str(right_range))
+        print("Obstacle Hit, Hit Point "+str(hit_point.x)+" "+str(hit_point.y))
         follow_wall()
 
         if(impossible == True):
